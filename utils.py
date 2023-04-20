393b212556d8db2e604d490a59252b974fd8b6aa
ferraroroberto <roberto.ferraro@gmail.com>
2023-04-20 13:34:22 +0200

    create another function to get the foreground windows explorer path, based on the window title

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import win32gui
+
 # Function to read the parameters from the txt file
 def read_params_from_txt_file(file_path):
     params = {}
@@ -7,3 +9,31 @@ def read_params_from_txt_file(file_path):
                 key, value = line.strip().split(" = ", 1)
                 params[key.strip()] = value.strip()
     return params
+
+# Function to get the path of the foreground windowx explorer: it onluy works if the explorer window has the full path showing
+def get_active_explorer_path():
+    # Get a list of all open windows
+    windows = []
+    win32gui.EnumWindows(lambda hwnd, windows: windows.append(hwnd), windows)
+
+    # Find the first window that has a path to a directory in its title, taking into account network drives too
+    explorer_handle = None
+    for hwnd in windows:
+        window_text = win32gui.GetWindowText(hwnd)
+        if ":\\" in window_text or window_text.startswith("\\\\S555"):
+            explorer_handle = hwnd
+            break
+
+    # If no Explorer window with a path in its title is found, print an error message and return None
+    if explorer_handle is None:
+        print("No Windows Explorer instance found with a path in its title.")
+        return None
+
+    # Get the window text of the selected Explorer window, which is the full path to the directory being displayed
+    folder_path = win32gui.GetWindowText(explorer_handle)
+
+    # Debugging output
+    print(f"Window handle: {explorer_handle}, Folder path: {folder_path}")
+
+    # Return the full path to the directory being displayed in the Explorer window
+    return folder_path
